205e7111cfdd11c577f7f4a584f0e77b15d52e76
Fabian Blum <fabian-blum@outlook.de>
2018-01-21 22:56:28 +0100

    PHP Mailer

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,9 +1,13 @@
 <?php
+
+require('phpmailer/PHPMailer.php');
+require('phpmailer/SMTP.php');
+
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "fabian-blum@outlook.de";
+    $email_subject = "Wolf-im-Blut Kontaktformular";
  
     function died($error) {
         // your error code can go here
@@ -18,46 +22,20 @@ if(isset($_POST['email'])) {
     // validation expected data exists
     if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['comments'])) {
+        !isset($_POST['subject']) ||
+        !isset($_POST['message'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
  
-    $name = $_POST['name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
  
     $email_message = "Form details below.\n\n";
- 
-     
+
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
@@ -65,22 +43,35 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "First Name: ".clean_string($name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email)."\n";
+    $email_message .= "Subject: ".clean_string($subject)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
  
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+
+    $mail = new PHPMailer(); 
+
+    $mail->IsSMTP(); 
+    $mail->Host     = 'smtp.strato.de'; 
+    $mail->SMTPAuth = true;  
+    $mail->Username = 'webmaster@wolf-im-blut.de'; 
+    $mail->Password = 'x';  
+
+    $mail->setFrom($email, $name);
+    $mail->Subject  = $subject;
+    $mail->Body     =  $message;
+    if(!$mail->send()) {
+      echo 'Message was not sent.';
+      echo 'Mailer error: ' . $mail->ErrorInfo;
+    } else {
+      echo 'Message has been sent.';
+    }
+
 ?>
  
 <!-- include your own success html here -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+Vielen Dank für Ihre Nachricht. Ich werde mich mit Ihnen zeitnah in Verbindung setzen.
  
 <?php
  
